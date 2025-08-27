92348f7bc92da0af49f401aa2700bbf3dfe0e11f
Cristhian Ivan Flores Hurtado <cfloresh@emeal.nttdata.com>
2025-08-27 15:42:48 -0500

    agregar cambios de dependencias xls a ExcelJS

--- tests/exampletest1LoginPom.spec.ts
+++ tests/exampletest1LoginPom.spec.ts
@@ -1,81 +1,41 @@
-import { test, expect } from '@playwright/test';
-import fs from 'fs';
-import path from 'path';
-import XLSX from 'xlsx';  
-import { LoginPageClass } from '../pageobject/LoginPage';
-import { CompraProcesoClass } from '../pageobject/CompraProceso';
-
-const excelFilePath = path.join(__dirname, '../data/DataDriven.xlsx');
-const fileBuffer = fs.readFileSync(excelFilePath);
-const workbook = XLSX.read(fileBuffer, { type: 'buffer' });
+import { test, expect } from '@playwright/test'
+import { LoginPageClass } from '../pageobject/LoginPage'
+import { CompraProcesoClass } from '../pageobject/CompraProceso'
+import { lecturaExcelData } from '../pageobject/lecturaDatos'
 
 
-/*test login FAIL */
-// Obtener hoja por nombre
-const sheetName2 = 'test1';  // nombre exacto de la hoja
-const worksheet2 = workbook.Sheets[sheetName2];
 
-if (!worksheet2) {
-  throw new Error(`La hoja con nombre "${sheetName2}" no existe en el archivo Excel.`);
-}
-const testData2: any[] = XLSX.utils.sheet_to_json(worksheet2, { defval: '' });
-test.describe('TEST 001 LOGIN FAIL', () => {
-  for (const data of testData2) {
-    test(`INPUT DATOS: ${data.user} ${data.pass}`, { tag: ['@regresion', '@LoginFail'] }, async ({ page }, testInfo) => {
-      await page.goto('/v1/')
-      await expect(page).toHaveTitle('Swag Labs')
-      const loginPageInterface = new LoginPageClass(page)
-      const pcinterface = new CompraProcesoClass(page)
-      await loginPageInterface.loginPageNOk(data.user,data.pass,testInfo)
-    });
-  }
+test('LOGIN FAIL',  { tag: ['@regresion', '@LoginFail'] },async ({ page } ,testInfo ) => {
+     // Cargar datos de Excel antes de la prueba
+    const datosexcel = await lecturaExcelData('test1', 2)
+    await page.goto('/v1/')
+    await expect(page).toHaveTitle('Swag Labs')
+    const loginPageInterface = new LoginPageClass(page)
+    const pcinterface = new CompraProcesoClass(page)
+    await loginPageInterface.loginPageNOk(datosexcel[1],datosexcel[2],testInfo)
+
 });
 
 
 
-
-/*test login OK */
-// Obtener hoja por nombre
-const sheetName3 = 'test3';  // nombre exacto de la hoja
-const worksheet3 = workbook.Sheets[sheetName3];
-
-if (!worksheet2) {
-  throw new Error(`La hoja con nombre "${sheetName3}" no existe en el archivo Excel.`);
-}
-const testData3: any[] = XLSX.utils.sheet_to_json(worksheet3, { defval: '' });
-test.describe('LOGIN SUCCESS', () => {
-  for (const data of testData3) {
-    test(`INPUT DATOS: ${data.user} ${data.pass}`, { tag: ['@regresion', '@LoginOK'] } , async ({ page }, testInfo) => {
+test('LOGIN OK',  { tag: ['@regresion', '@LoginOK'] },async ({ page } ,testInfo ) => {
+      const datosexcel = await lecturaExcelData('test3', 2)
       await page.goto('/v1/')
       await expect(page).toHaveTitle('Swag Labs')
       const loginPageInterface = new LoginPageClass(page)
       const pcinterface = new CompraProcesoClass(page)
-      await loginPageInterface.loginPageOk(data.user,data.pass,testInfo)
-    });
-  }
+      await loginPageInterface.loginPageOk(datosexcel[1],datosexcel[2],testInfo)
+
 });
 
 
-
-/*TEST PROCESO DE COMPRA COMPLETO */
-// Obtener hoja por nombre
-const sheetName = 'test2';  // nombre exacto de la hoja
-const worksheet = workbook.Sheets[sheetName];
-if (!worksheet) {
-  throw new Error(`La hoja con nombre "${sheetName}" no existe en el archivo Excel.`);
-}
-const testData: any[] = XLSX.utils.sheet_to_json(worksheet, { defval: '' });
-test.describe('FLUJO DE COMPRAS COMPLETO', () => {
-  for (const data of testData) {
-    test(`INPUT DATOS: ${data.firstName} ${data.lastName} ${data.postalCode} ${data.user} ${data.pass}`, { tag: ['@regresion', '@flujoCompleto'] } , async ({ page }, testInfo) => {
+test('PROCESO DE COMPRA',  { tag: ['@regresion', '@ProcesoCompra'] },async ({ page } ,testInfo ) => {
+      const datosexcel = await lecturaExcelData('test2', 2)
       await page.goto('/v1/')
       await expect(page).toHaveTitle('Swag Labs')
       const loginPageInterface = new LoginPageClass(page)
       const pcinterface = new CompraProcesoClass(page)
-      await loginPageInterface.loginPageOk(data.user,data.pass,testInfo)
-      await pcinterface.procesocompra(data.firstName, data.lastName, data.postalCode, testInfo)
-    });
-  }
+      await loginPageInterface.loginPageOk(datosexcel[4],datosexcel[5],testInfo)
+      await pcinterface.procesocompra(datosexcel[1],datosexcel[2],datosexcel[3], testInfo)
 });
-
 
