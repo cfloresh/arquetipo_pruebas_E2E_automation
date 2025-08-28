fde02ba21051675a541d12f0b23d9c30bfee4503
Cristhian Ivan Flores Hurtado <cfloresh@emeal.nttdata.com>
2025-08-28 14:12:21 -0500

    AGREGAR ARCHIVO DE DEPENDENCIAS FINALES

--- pageobject/lecturaDatos.ts
+++ pageobject/lecturaDatos.ts
@@ -1,13 +1,13 @@
-import ExcelJS from 'exceljs';
+import ExcelJS from 'exceljs'
 
 // Función asíncrona para cargar datos desde Excel
 export async function lecturaExcelData(sheetName: string, rowNumber: number) {
-    const workbook = new ExcelJS.Workbook();
-    await workbook.xlsx.readFile('./data/DataDriven.xlsx');
-    const worksheet = workbook.getWorksheet(sheetName);
+    const workbook = new ExcelJS.Workbook()
+    await workbook.xlsx.readFile('./data/DataDriven.xlsx')
+    const worksheet = workbook.getWorksheet(sheetName)
     if (!worksheet) {
-        throw new Error(`La hoja con nombre "${sheetName}" no existe en el archivo Excel.`);
+        throw new Error(`La hoja con nombre "${sheetName}" no existe en el archivo Excel.`)
     }
-    const rowValues = worksheet.getRow(rowNumber).values;
-    return rowValues;
+    const rowValues = worksheet.getRow(rowNumber).values
+    return rowValues
 }
